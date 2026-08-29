cd172b8fa0e9f4bd2d0d3978e696d1b7c722f345
ViditSikarwar <f20250102@pilani.bits-pilani.ac.in>
2026-08-29 18:10:40 +0000

    lab01 task3

--- labs/lab01/task3/rca.v
+++ labs/lab01/task3/rca.v
@@ -10,6 +10,22 @@
 // TODO: instantiate four FA_Gate instances, same chaining pattern as
 // Task 2 (FA0..FA3, carry chain c1,c2,c3).
 
+module FA_Gate(
+  input  a,
+  input  b,
+  input  cin,
+  output sum,
+  output cout
+);
+  wire ps, pc1, pc2;
+  xor  #(2) (ps,   a,   b);
+  and  #(2) (pc1,  a,   b);
+  xor  #(2) (sum,  cin, ps);
+  and  #(2) (pc2,  cin, ps);
+  or   #(2) (cout, pc1, pc2);
+
+endmodule
+
 module rca(
   input  [3:0] a,
   input  [3:0] b,
@@ -20,6 +36,34 @@ module rca(
 
   wire c1, c2, c3;
 
-  // TODO: your four FA_Gate instances go here.
+ FA_Gate FA0 (
+    .a    (a[0]),
+    .b    (b[0]),
+    .cin  (cin),
+    .sum  (sum[0]),
+    .cout (c1)
+  );
+  FA_Gate FA1 (
+    .a    (a[1]),
+    .b    (b[1]),
+    .cin  (c1),
+    .sum  (sum[1]),
+    .cout (c2)
+  );
+  FA_Gate FA2 (
+    .a    (a[2]),
+    .b    (b[2]),
+    .cin  (c2),
+    .sum  (sum[2]),
+    .cout (c3)
+  );
+  FA_Gate FA3 (
+    .a    (a[3]),
+    .b    (b[3]),
+    .cin  (c3),
+    .sum  (sum[3]),
+    .cout (cout)
+  );
+
 
 endmodule
